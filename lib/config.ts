20e0000e69f466b88f14e6fb4b7e71ce9b040ad4
River <52377340+Shydlock@users.noreply.github.com>
2022-09-04 13:16:26 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -61,6 +61,7 @@ export const linkedin: string | null = getSiteConfig('linkedin', null)
 export const newsletter: string | null = getSiteConfig('newsletter', null)
 export const zhihu: string | null = getSiteConfig('zhihu', null)
 export const weibo: string | null = getSiteConfig('weibo', null)
+export const rss: string | null = getSiteConfig('rss', null)
 
 // default notion values for site-wide consistency (optional; may be overridden on a per-page basis)
 export const defaultPageIcon: string | null = getSiteConfig(
