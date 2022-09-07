4d23342b768c965c8e2c9935a11f89e26de48913
River <52377340+Shydlock@users.noreply.github.com>
2022-09-07 15:25:08 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '36eeec2061e04fa39bab4c6222927a2e',
- 
+  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
